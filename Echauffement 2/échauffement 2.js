9fffb7095c5977abe0514b64497162ca50192b8a
S-Abric <52480274+S-Abric@users.noreply.github.com>
2019-08-23 23:37:16 +0200

    Add files via upload

---
+++ Echauffement 2/échauffement 2.js
@@ -0,0 +1,19 @@
+var cheval;
+
+while (true) {
+	cheval = prompt ("Quelle est la couleur du cheval blanc d'Henri IV?");
+	if (cheval === 'blanc'){
+		break;
+	}
+	alert ("Tu le fais exprès?!");
+}
+
+var nain
+
+while (true) {
+	nain = prompt ("Combien il y a de 7 nains?");
+	if (nain === '7'){
+		break;
+	}
+	alert ("T'es débiles? Recommence");
+}
